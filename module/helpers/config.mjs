75e37eb4d8a80f0cff5b8631129deb619cb5f001
yoloxenox <jean@guerci.fr>
2023-11-26 01:26:46 +0100

    initial commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,60 @@
+export const AELYRIA = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ AELYRIA.abilities = {
+  "dex": "AELYRIA.AbilityDex",
+  "agi": "AELYRIA.AbilityAgi",
+  "perc": "AELYRIA.AbilityPerc",
+  "str": "AELYRIA.AbilityStr",
+  "cons": "AELYRIA.AbilityCons",
+  "char": "AELYRIA.AbilityChar",
+  "int": "AELYRIA.AbilityInt",
+  "men": "AELYRIA.AbilityMen",
+  "pow": "AELYRIA.AbilityPow"
+};
+
+AELYRIA.abilityAbbreviations = {
+  "dex": "AELYRIA.AbilityDexAbbr",
+  "agi": "AELYRIA.AbilityAgiAbbr",
+  "perc": "AELYRIA.AbilityPercAbbr",
+  "str": "AELYRIA.AbilityStrAbbr",
+  "cons": "AELYRIA.AbilityConsAbbr",
+  "char": "AELYRIA.AbilityCharAbbr",
+  "int": "AELYRIA.AbilityIntAbbr",
+  "men": "AELYRIA.AbilityMenAbbr",
+  "pow": "AELYRIA.AbilityPowAbbr"
+};
+
+AELYRIA.skills = {
+  "athletisme": "AELYRIA.SkillAthletisme",
+  "acrobatie": "AELYRIA.SkillAcrobatie",
+  "arme1": "AELYRIA.SkillArme1",
+  "arme2": "AELYRIA.SkillArme2",
+  "arme3": "AELYRIA.SkillArme3",
+  "artisanat1": "AELYRIA.SkillArtisanat1",
+  "artisanat2": "AELYRIA.SkillArtisanat2",
+  "artisanat3": "AELYRIA.SkillArtisanat3",
+  "bricolage": "AELYRIA.SkillBricolage",
+  "commandement": "AELYRIA.SkillCommandement",
+  "concentration": "AELYRIA.SkillConcentration",
+  "connaissance": "AELYRIA.SkillConnaissance",
+  "crochetage": "AELYRIA.SkillCrochetage",
+  "detectionMagique": "AELYRIA.SkillDetectionMagique",
+  "esquive": "AELYRIA.SkillEsquive",
+  "furtivite": "AELYRIA.SkillFurtivite",
+  "intimidation": "AELYRIA.SkillIntimidation",
+  "mystere": "AELYRIA.SkillMystere",
+  "negociation": "AELYRIA.SkillNegociation",
+  "observation": "AELYRIA.SkillObservation",
+  "parade": "AELYRIA.SkillParade",
+  "passePasse": "AELYRIA.SkillPassePasse",
+  "precisionMagique": "AELYRIA.SkillPrecisionMagique",
+  "premiersSoins": "AELYRIA.SkillPremiersSoins",
+  "pugilat": "AELYRIA.SkillPugilat",
+  "seduction": "AELYRIA.SkillSeduction",
+  "subterfuge": "AELYRIA.SkillSubterfuge",
+  "vigilance": "AELYRIA.SkillVigilance"
+};
